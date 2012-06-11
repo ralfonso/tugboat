c6f9ff94b560072e3d1c2928d123061649082336
Ryan Roemmich <ryan@weareinstrument.com>
2012-06-11 11:07:08 -0700

    add deps

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :main tugboat.core
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.1.2"]
-                 [clj-redis "0.0.12"]])
+                 [clj-redis "0.0.12"]
+                 [com.eaio.uuid/uuid "3.2"]])
