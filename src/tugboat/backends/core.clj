11d258ac64c0578a369db3893c04c47a74bd1f74
Ryan Roemmich <ryan@weareinstrument.com>
2012-06-11 11:08:34 -0700

    comments

--- src/tugboat/backends/core.clj
+++ src/tugboat/backends/core.clj
@@ -3,14 +3,16 @@
 
 (defn load-backend
   [config]
-    (let [backend-type (:type (:backend config))
-          backend-ns (str "tugboat.backends." (name backend-type))]
-      (try 
-        (let [backend-sym (symbol backend-ns)]
-          (do
-            (require backend-sym)
-            ((ns-resolve backend-sym (symbol "create-adapter")) config)))
-        (catch FileNotFoundException e (throw (Exception. (format "Unable to load Tugboat backend: %s (%s)" backend-ns e)))))))
+  (let [backend-type (:type (:backend config))
+        backend-ns (str "tugboat.backends." (name backend-type))]
+    (try 
+      (let [backend-sym (symbol backend-ns)]
+        (do
+          (require backend-sym)
+
+          ;; FIXME call create-adapter on our adapter, this resolution code smells
+          ((ns-resolve backend-sym (symbol "create-adapter")) config)))
+      (catch FileNotFoundException e (throw (Exception. (format "Unable to load Tugboat backend: %s (%s)" backend-ns e)))))))
 
 (defn create
   [config]
